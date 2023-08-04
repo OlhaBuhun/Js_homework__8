81c13324cfdb7765e1d38e118eee7332e7d0bca3
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-08-04 18:42:07 +0300

    Solution of the second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,32 @@
 import Player from '@vimeo/player';
-console.log(Player);
 import  throttle  from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
-console.log(iframe);
+
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
+let secondPlay = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+const onPlay = function(data) {
+  // data is an object containing properties specific to that event
+  localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+
+player.setCurrentTime(secondPlay).then(function(seconds) {
+  // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+      default:
+          // some other error occurred
+          break;
+  }
 });
+
+player.on('pause', throttle(onPlay, 1000));
 
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
